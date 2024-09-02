009636d7d48da26fdb707a8bb293d98d09abb0a4
Eunchan Jeong <jec65147@gmail.com>
2024-09-02 15:55:09 +0900

    :sparkles: Feat: 진행중인 콘테스트 게시글 등록 기능 구현

--- src/main/java/com/oasis/hworld/contest/controller/ContestController.java
+++ src/main/java/com/oasis/hworld/contest/controller/ContestController.java
@@ -1,11 +1,15 @@
 package com.oasis.hworld.contest.controller;
 
+import com.oasis.hworld.cart.dto.CartItemRequestDTO;
+import com.oasis.hworld.common.dto.CommonResponseDTO;
 import com.oasis.hworld.contest.dto.CoordinationResponseDTO;
 import com.oasis.hworld.contest.dto.PostDetailResponseDTO;
+import com.oasis.hworld.contest.dto.PostRequestDTO;
 import com.oasis.hworld.contest.dto.PostSummaryDTO;
 import com.oasis.hworld.contest.service.ContestService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j;
+import oracle.ucp.proxy.annotation.Post;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -22,7 +26,8 @@ import java.util.List;
  * 수정일        수정자        수정내용
  * ----------  --------    ------------------------------------------------------
  * 2024.08.31  	정은찬        최초 생성
- * 2024.09.01   정은찬        파라미터를 통해 콘테스트 게시글 목록 조회 통합, 게시글 상세 조회 메소드 추가
+ * 2024.09.01   정은찬        파라미터를 통해 콘테스트 게시글 목록 조회 메소드 통합, 게시글 상세 조회 메소드 추가
+ * 2024.09.02   정은찬        코디 목록 조회 메소드, 진행중인 콘테스트 게시글 등록 메소드 추가
  * </pre>
  */
 @RestController
@@ -65,4 +70,19 @@ public class ContestController {
     public ResponseEntity<List<CoordinationResponseDTO>> getCoordinationList(@PathVariable("memberId") int memberId) {
         return ResponseEntity.ok(service.getCoordinationList(memberId));
     }
+
+    /**
+     * 진행중인 콘테스트 게시글 등록
+     *
+     * @author 정은찬
+     * @apiNote 진행중인 콘테스트 게시글을 등록한다.
+     */
+    @PostMapping("/posts")
+    ResponseEntity<CommonResponseDTO> addContestPost(@RequestBody PostRequestDTO postRequestDTO) {
+        // todo : memberId 로직 추가
+        return service.addContestPost(7, postRequestDTO) ?
+                ResponseEntity.ok(new CommonResponseDTO(true, "콘테스트 게시글이 등록되었습니다.")) :
+                ResponseEntity.ok(new CommonResponseDTO(false, "이미 같은 코디의 게시글이 있습니다."));
+    }
 }
+
--- src/main/java/com/oasis/hworld/contest/service/ContestServiceImpl.java
+++ src/main/java/com/oasis/hworld/contest/service/ContestServiceImpl.java
@@ -24,7 +24,7 @@ import static com.oasis.hworld.common.exception.ErrorCode.*;
  * ----------  --------    ------------------------------------------------------
  * 2024.08.31  	정은찬        최초 생성
  * 2024.09.01   정은찬        파라미터를 통해 콘테스트 게시글 목록 조회 메소드 통합, 게시글 상세 조회 메소드 추가
- * 2024.09.02   정은찬        회원 ID를 통한 코디 목록 조회 메소드 추가
+ * 2024.09.02   정은찬        회원 ID를 통한 코디 목록 조회 메소드, 진행중인 콘테스트 게시글 등록 메소드 추가
  * </pre>
  */
 @Service
@@ -70,5 +70,21 @@ public class ContestServiceImpl implements ContestService {
         List<CoordinationResponseDTO> coordinationList = mapper.selectCoordinationListByMemberId(memberId);
 
         return coordinationList;
+    }
+
+    /**
+     * 진행중인 콘테스트 게시글 등록
+     *
+     * @author 정은찬
+     */
+    public boolean addContestPost(int memberId, PostRequestDTO postRequestDTO) {
+        int coordinationId = postRequestDTO.getCoordinationId();
+
+        // 이미 같은 코디 게시글 존재
+        if(mapper.selectContestPostByMemberIdAndCoordinationId(memberId, coordinationId) != null) {
+            return false;
+        }
+        
+       return mapper.insertContestPost(memberId, postRequestDTO) == 1;
     }
 }
